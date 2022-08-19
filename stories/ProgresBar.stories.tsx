1cd24e4ab99bbb8ebc02fac378113e48504e0ee7
Dayanne <dayanne.carolina@findup.com.br>
2022-08-19 10:31:55 -0300

    Finalização do progressbar

---
+++ stories/ProgresBar.stories.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ProgressBar } from '../src/components/ProgressBar';
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Data Display/ProgressBar',
+  component: ProgressBar,
+
+  args: {
+    //👇 Now all Button stories will be primary.
+  },
+  argTypes: {
+   progress:{
+    description: "Porcentagem do progresso em formato de 0 a 100."
+   }
+  }
+} as ComponentMeta<typeof ProgressBar>;
+
+
+//👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof ProgressBar> = (args) => <ProgressBar {...args} />;
+
+//👇 Each story then reuses that template
+export const Default = Template.bind({});
+Default.args = { progress: 0 };
