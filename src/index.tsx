d4dc7505d2e2f7a6cdaaa3cc915b1ad9535e878f
Italo Gama <italoflorencio@gmail.com>
2022-09-12 17:14:45 -0300

    Adição de export do CardClean

--- src/index.tsx
+++ src/index.tsx
@@ -28,4 +28,5 @@ export { Tooltip } from './components/Tooltip';
 export { Popover } from './components/Popover';
 export { List } from './components/List';
 export { Table } from './components/Table';
+export { CardClean } from './components/CardClean';
 
