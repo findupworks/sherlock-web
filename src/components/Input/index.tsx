9bb7a1a2392c3f11f46fb73fa6a9476e271017d6
Italo Gama <italoflorencio@gmail.com>
2022-09-22 09:51:03 -0300

    Correção do type password no input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,197 +1,196 @@
-import React, { useState, InputHTMLAttributes } from 'react';
-import classNames from 'classnames';
+import React, { useState, InputHTMLAttributes } from "react";
+import classNames from "classnames";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { ColorVariant, InputTypeVariant  } from '../../types';
+import { ColorVariant, InputTypeVariant } from "../../types";
 
 export interface SelectorItem {
-    label?: string;
-    value?: string;
+  label?: string;
+  value?: string;
 }
 
-
-export type IProps = InputHTMLAttributes<HTMLInputElement> &  {
-    label?: string;
-    inputType: InputTypeVariant;
-    selectorItems?: SelectorItem[],
-    colorVariant?: ColorVariant,
-    tag?: boolean;
-    icon?: string;
-    isVisible?: boolean;
-    onClickIcon?: () => void;
-}
+export type IProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  inputType: InputTypeVariant;
+  selectorItems?: SelectorItem[];
+  colorVariant?: ColorVariant;
+  tag?: boolean;
+  icon?: string;
+  isVisible?: boolean;
+  onClickIcon?: () => void;
+};
 
 export const Input: React.FC<IProps> = ({
-    label,
-    inputType = "default",
-    colorVariant,
-    selectorItems,
-    tag,
-    icon,
-    isVisible,
-    onClickIcon,
-    ...props
- }) => {
+  label,
+  inputType = "default",
+  colorVariant,
+  selectorItems,
+  tag,
+  icon,
+  isVisible,
+  onClickIcon,
+  ...props
+}) => {
+  const [toggleIcon, setToggleIcon] = useState("eye-slash");
+  const [typeInput, setTypeInput] = useState("");
 
-    const [toggleIcon, setToggleIcon] = useState('eye-slash');
-    const [typeInput, setTypeInput] = useState('password');
-
-    function changleToggleIcon(){
-        if(toggleIcon == 'eye'){
-            setTypeInput('password');
-            setToggleIcon('eye-slash');
-        } else {
-            setTypeInput('text');
-            setToggleIcon('eye');
-        }
+  function changleToggleIcon() {
+    if (toggleIcon == "eye") {
+      setTypeInput("password");
+      setToggleIcon("eye-slash");
+    } else {
+      setTypeInput("text");
+      setToggleIcon("eye");
     }
+  }
 
-    function getTag(){
-        return (
-            colorVariant == "success" || colorVariant == "danger"  || colorVariant == "warning"?
-            <div className="flex items-center">
-                <i className={
-                    classNames(
-                        "fa", {
-                        "fa-circle-check text-success" : 'success' == colorVariant,
-                        "fa-warning text-warning" : 'warning' == colorVariant,
-                        "fa-circle-exclamation text-danger" : 'danger' == colorVariant,
-                    })}>
-                </i>
-                <label className="block mb-2 pl-2 pt-2 text-sm font-medium text-gray-900 dark:text-dark">
-                    { 'success' == colorVariant ? 'Sucesso' : '' }
-                    { 'danger' == colorVariant ? 'Erro' : '' }
-                    { 'warning' == colorVariant ? 'Cuidado' : '' }
-                </label>
-            </div> 
-            : ''
-        )
-    }
+  function getTag() {
+    return colorVariant == "success" || colorVariant == "danger" || colorVariant == "warning" ? (
+      <div className="flex items-center">
+        <i
+          className={classNames("fa", {
+            "fa-circle-check text-success": "success" == colorVariant,
+            "fa-warning text-warning": "warning" == colorVariant,
+            "fa-circle-exclamation text-danger": "danger" == colorVariant,
+          })}
+        ></i>
+        <label className="block mb-2 pl-2 pt-2 text-sm font-medium text-gray-900 dark:text-dark">
+          {"success" == colorVariant ? "Sucesso" : ""}
+          {"danger" == colorVariant ? "Erro" : ""}
+          {"warning" == colorVariant ? "Cuidado" : ""}
+        </label>
+      </div>
+    ) : (
+      ""
+    );
+  }
 
-    function getContentByType() {
-        return <>
-                {
-                    'textarea' === inputType 
-                    ? 
-                    <div className="flex flex-col justify-center">
-                        <div>
-                            { label ? <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">{ label }</label> : '' }
-                            <textarea className={
-                                    classNames(
-                                        'resize w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-clip-padding border border-solid rounded transition ease-in-out m-0', {
-                                        'bg-gray-50 border border-gray-300' : 'default' == colorVariant,
-                                        'border-success bg-successL1 bg-opacity-10' : 'success' == colorVariant,
-                                        'border-warning bg-warningL1 bg-opacity-10': 'warning' == colorVariant,
-                                        'border-danger bg-dangerL1 bg-opacity-10' : 'danger' == colorVariant,
-                                        'border-info bg-infoL1 bg-opacity-10' : 'info' == colorVariant,
-                                        'border-primary bg-primaryL1 bg-opacity-10' : 'primary' == colorVariant,
-                                        'border-secondary bg-secondaryL1 bg-opacity-10' : 'secondary' == colorVariant,
-                                    })}
-                            ></textarea>
-                        </div>
-                        {
-                            tag ? getTag() : ''
-                        }
-                    </div>
-                    :
-                    <div className="relative">
-                            {
-                                'icon' == inputType ?
-                                <>
-                                { label ? <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">{ label }</label> : ''}
-                                <div className="flex  justify-between">
-                                    <input type="text" className={
-                                        classNames(
-                                            "border text-gray-900 text-sm rounded-md block w-full p-3", {
-                                            'bg-gray-50 border border-gray-300' : 'default' == colorVariant,
-                                            'border-success bg-successL1 bg-opacity-10' : 'success' == colorVariant,
-                                            'border-warning bg-warningL1 bg-opacity-10': 'warning' == colorVariant,
-                                            'border-danger bg-dangerL1 bg-opacity-10' : 'danger' == colorVariant,
-                                            'border-info bg-infoL1 bg-opacity-10' : 'info' == colorVariant,
-                                            'border-primary bg-primaryL1 bg-opacity-10' : 'primary' == colorVariant,
-                                            'border-secondary bg-secondaryL1 bg-opacity-10' : 'secondary' == colorVariant,
-                                        })}
-                                        {...props}
-                                    />
-                                    <i onClick={onClickIcon} className={
-                                        classNames(
-                                            `absolute  right-4 top-10 text-gray-600 fa fa-${icon}`, {
-                                        })}></i>
-                                </div>
-                                {
-                                    tag ? getTag() : ''
-                                }
-                                </>
-                                : ''
-                            }
-                            {
-                                'selector' == inputType ? 
-                                <>
-                                    <div>
-                                        { label ? <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{ label }</label> : '' }
-                                        <select className={
-                                                classNames(
-                                                    'text-gray-900 text-sm border rounded-lg block w-full p-2.5', {
-                                                    'bg-gray-50 border border-gray-300' : 'default' == colorVariant,
-                                                    'border-success bg-successL1 bg-opacity-10' : 'success' == colorVariant,
-                                                    'border-warning bg-warningL1 bg-opacity-10': 'warning' == colorVariant,
-                                                    'border-danger bg-dangerL1 bg-opacity-10' : 'danger' == colorVariant,
-                                                    'border-info bg-infoL1 bg-opacity-10' : 'info' == colorVariant,
-                                                    'border-primary bg-primaryL1 bg-opacity-10' : 'primary' == colorVariant,
-                                                    'border-secondary bg-secondaryL1 bg-opacity-10' : 'secondary' == colorVariant,
-                                                })}
-                                        >
-                                            <option defaultValue={'-'} className="text-xs">-</option>
-                                            {
-                                                selectorItems ?
-                                                selectorItems.map(item => (
-                                                    <option value={item.value}>{item.label}</option>
-
-                                                ))
-                                                : ''
-                                            }
-                                        </select>
-                                    </div>
-                                </>
-                                : ''
-                            }
-                    </div>
-                }
-            </>
-    }
-
-    function getContentDefault() {
-        return <div>
-                    { label ? <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">{ label }</label> : ''}
-                    <div className={
-                            classNames(
-                                "flex relative justify-between", {
-                                
-                        })}>
-                        <input type={typeInput} className={
-                            classNames(
-                                "border text-dark text-sm rounded-lg block w-full p-3", {
-                                'bg-gray-50 border border-gray-300' : 'default' == colorVariant,
-                                'border-success bg-successL1 bg-opacity-10' : 'success' == colorVariant,
-                                'border-warning bg-warningL1 bg-opacity-10': 'warning' == colorVariant,
-                                'border-danger bg-dangerL1 bg-opacity-10' : 'danger' == colorVariant,
-                                'border-info bg-infoL1 bg-opacity-10' : 'info' == colorVariant,
-                                'border-primary bg-primaryL1 bg-opacity-10' : 'primary' == colorVariant,
-                                'border-secondary bg-secondaryL1 bg-opacity-10' : 'secondary' == colorVariant,
-                            })}
-                            {...props}
-                        />
-                        { isVisible ?
-                            <i onClick={changleToggleIcon} className={`fa fa-${toggleIcon} right-3 top-3 text-lg absolute text-gray-600`}></i>
-                            : ''
-                        }
-                    </div>
-                    {
-                        tag ? getTag() : ''
-                    }
-                </div>
-        }
-
+  function getContentByType() {
     return (
-        'default' == inputType ? getContentDefault() : getContentByType()
-      );
-}
+      <>
+        {"textarea" === inputType ? (
+          <div className="flex flex-col justify-center">
+            <div>
+              {label ? (
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">{label}</label>
+              ) : (
+                ""
+              )}
+              <textarea
+                className={classNames(
+                  "resize w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-clip-padding border border-solid rounded transition ease-in-out m-0",
+                  {
+                    "bg-gray-50 border border-gray-300": "default" == colorVariant,
+                    "border-success bg-successL1 bg-opacity-10": "success" == colorVariant,
+                    "border-warning bg-warningL1 bg-opacity-10": "warning" == colorVariant,
+                    "border-danger bg-dangerL1 bg-opacity-10": "danger" == colorVariant,
+                    "border-info bg-infoL1 bg-opacity-10": "info" == colorVariant,
+                    "border-primary bg-primaryL1 bg-opacity-10": "primary" == colorVariant,
+                    "border-secondary bg-secondaryL1 bg-opacity-10": "secondary" == colorVariant,
+                  }
+                )}
+              ></textarea>
+            </div>
+            {tag ? getTag() : ""}
+          </div>
+        ) : (
+          <div className="relative">
+            {"icon" == inputType ? (
+              <>
+                {label ? (
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">{label}</label>
+                ) : (
+                  ""
+                )}
+                <div className="flex  justify-between">
+                  <input
+                    type="text"
+                    className={classNames("border text-gray-900 text-sm rounded-md block w-full p-3", {
+                      "bg-gray-50 border border-gray-300": "default" == colorVariant,
+                      "border-success bg-successL1 bg-opacity-10": "success" == colorVariant,
+                      "border-warning bg-warningL1 bg-opacity-10": "warning" == colorVariant,
+                      "border-danger bg-dangerL1 bg-opacity-10": "danger" == colorVariant,
+                      "border-info bg-infoL1 bg-opacity-10": "info" == colorVariant,
+                      "border-primary bg-primaryL1 bg-opacity-10": "primary" == colorVariant,
+                      "border-secondary bg-secondaryL1 bg-opacity-10": "secondary" == colorVariant,
+                    })}
+                    {...props}
+                  />
+                  <i
+                    onClick={onClickIcon}
+                    className={classNames(`absolute  right-4 top-10 text-gray-600 fa fa-${icon}`, {})}
+                  ></i>
+                </div>
+                {tag ? getTag() : ""}
+              </>
+            ) : (
+              ""
+            )}
+            {"selector" == inputType ? (
+              <>
+                <div>
+                  {label ? (
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">{label}</label>
+                  ) : (
+                    ""
+                  )}
+                  <select
+                    className={classNames("text-gray-900 text-sm border rounded-lg block w-full p-2.5", {
+                      "bg-gray-50 border border-gray-300": "default" == colorVariant,
+                      "border-success bg-successL1 bg-opacity-10": "success" == colorVariant,
+                      "border-warning bg-warningL1 bg-opacity-10": "warning" == colorVariant,
+                      "border-danger bg-dangerL1 bg-opacity-10": "danger" == colorVariant,
+                      "border-info bg-infoL1 bg-opacity-10": "info" == colorVariant,
+                      "border-primary bg-primaryL1 bg-opacity-10": "primary" == colorVariant,
+                      "border-secondary bg-secondaryL1 bg-opacity-10": "secondary" == colorVariant,
+                    })}
+                  >
+                    <option defaultValue={"-"} className="text-xs">
+                      -
+                    </option>
+                    {selectorItems ? selectorItems.map((item) => <option value={item.value}>{item.label}</option>) : ""}
+                  </select>
+                </div>
+              </>
+            ) : (
+              ""
+            )}
+          </div>
+        )}
+      </>
+    );
+  }
+
+  function getContentDefault() {
+    return (
+      <div>
+        {label ? <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">{label}</label> : ""}
+        <div className={classNames("flex relative justify-between", {})}>
+          <input
+            type={typeInput}
+            className={classNames("border text-dark text-sm rounded-lg block w-full p-3", {
+              "bg-gray-50 border border-gray-300": "default" == colorVariant,
+              "border-success bg-successL1 bg-opacity-10": "success" == colorVariant,
+              "border-warning bg-warningL1 bg-opacity-10": "warning" == colorVariant,
+              "border-danger bg-dangerL1 bg-opacity-10": "danger" == colorVariant,
+              "border-info bg-infoL1 bg-opacity-10": "info" == colorVariant,
+              "border-primary bg-primaryL1 bg-opacity-10": "primary" == colorVariant,
+              "border-secondary bg-secondaryL1 bg-opacity-10": "secondary" == colorVariant,
+            })}
+            {...props}
+          />
+          {isVisible ? (
+            <i
+              onClick={changleToggleIcon}
+              className={`fa fa-${toggleIcon} right-3 top-3 text-lg absolute text-gray-600`}
+            ></i>
+          ) : (
+            ""
+          )}
+        </div>
+        {tag ? getTag() : ""}
+      </div>
+    );
+  }
+
+  return "default" == inputType ? getContentDefault() : getContentByType();
+};
--- src/types.tsx
+++ src/types.tsx
@@ -1,18 +1,43 @@
-export type ColorVariant = 'default' | 'primary' | 'secondary' | 'success' | 'info' | 'danger' | 'warning';
-export type SizeVariant = 'default'| 'sm' | 'md' | 'lg';
-export type CardVariant = 'default' | 'iconCard' | 'userCard' | 'horizontalCard';
-export type DropwdonVariant = 'default' | 'icon' | 'radio' | 'checkbox' | 'filter';
-export type InputDropdownVariant = 'radio' | 'checkbox';
-export type DatePickerVariant = 'datetime' | 'date' | 'time';
-export type EmptyStateVariant = 'default' | 'noImage';
-export type InputTypeVariant = 'default' | 'textarea' | 'selector' | 'icon';
-export type TypeOptionsVariant = 'stepCircle' | 'stepCircleCondensed' | 'stepBar';
-export type PaginationVariant = 'default' | 'vertical';
-export type StickerSizeVariant = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-export type SwitchSize = 'sm' | 'md';
-export type PopoverDirection = 'right' | 'left' | 'top' | 'bottom';
-export type TooltipDirection = 'right' | 'right-start' | 'right-end' | 'left' | 'left-start' | 'left-end' | 'top' | 'top-start' | 'top-end' | 'bottom' | 'bottom-start' | 'bottom-end';
-export type ListVariant = 'group' | 'divider'
-export type TableTdVariant = 'blank' | 'checkbox' | 'progressbar' | 'text' | 'textSticker' | 'label' | 'button' | 'sticker' | 'input' | 'action' | 'badge' | 'toggle' | 'icon'
-export type NavBarVariant = 'light' | 'dark' 
-export type ProgressBarTypeKanbanVariant = 'progress' | 'error' | 'success' 
+export type ColorVariant = "default" | "primary" | "secondary" | "success" | "info" | "danger" | "warning";
+export type SizeVariant = "default" | "sm" | "md" | "lg";
+export type CardVariant = "default" | "iconCard" | "userCard" | "horizontalCard";
+export type DropwdonVariant = "default" | "icon" | "radio" | "checkbox" | "filter";
+export type InputDropdownVariant = "radio" | "checkbox";
+export type DatePickerVariant = "datetime" | "date" | "time";
+export type EmptyStateVariant = "default" | "noImage";
+export type InputTypeVariant = "default" | "textarea" | "selector" | "icon" | "password";
+export type TypeOptionsVariant = "stepCircle" | "stepCircleCondensed" | "stepBar";
+export type PaginationVariant = "default" | "vertical";
+export type StickerSizeVariant = "xs" | "sm" | "md" | "lg" | "xl";
+export type SwitchSize = "sm" | "md";
+export type PopoverDirection = "right" | "left" | "top" | "bottom";
+export type TooltipDirection =
+  | "right"
+  | "right-start"
+  | "right-end"
+  | "left"
+  | "left-start"
+  | "left-end"
+  | "top"
+  | "top-start"
+  | "top-end"
+  | "bottom"
+  | "bottom-start"
+  | "bottom-end";
+export type ListVariant = "group" | "divider";
+export type TableTdVariant =
+  | "blank"
+  | "checkbox"
+  | "progressbar"
+  | "text"
+  | "textSticker"
+  | "label"
+  | "button"
+  | "sticker"
+  | "input"
+  | "action"
+  | "badge"
+  | "toggle"
+  | "icon";
+export type NavBarVariant = "light" | "dark";
+export type ProgressBarTypeKanbanVariant = "progress" | "error" | "success";
