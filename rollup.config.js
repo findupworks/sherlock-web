ecf92c72efb45f2fe18be8e4acdd2bc53f75b595
Italo Gama <italoflorencio@gmail.com>
2022-11-03 17:38:37 -0300

    Ajuste config do rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,35 @@ import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
-// import external from "rollup-plugin-peer-deps-external";
+import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
 
-export default {
-  input: "src/index.tsx",
-  output: {
-    file: "dist/index.mjs",
+export default [
+  {
+    input: "./src/index.tsx",
+    output: [
+      { file: "dist/index.js", format: "cjs" },
+      { file: "dist/index.es.js", format: "es", exports: "named" },
+    ],
+    plugins: [
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+        babelHelpers: "bundled",
+      }),
+      external(),
+      resolve({ browser: true }),
+      typescript({ module: "ESNext" }),
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      commonjs({
+        include: /node_modules/,
+        requireReturnsDefault: "auto", // <---- this solves default issue
+      }),
+      terser(),
+    ],
   },
-  plugins: [
-    typescript({ module: "ESNext" }),
-    postcss({
-      plugins: [],
-      minimize: true,
-    }),
-    babel({
-      exclude: "node_modules/**",
-      presets: ["@babel/preset-react"],
-    }),
-    //external(),
-    //resolve(),
-    terser(),
-  ],
-};
+];
